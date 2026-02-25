0fec2910392a571b124f621555213a95d1f33f8b
Pranav Junare <pranavjunare7201@gmail.com>
2026-02-25 22:26:02 +0530

    Added project

---
+++ src/main/java/com/_1zko/hacknovahackathon26backend/HacknovaHackathon26BackendApplication.java
@@ -0,0 +1,13 @@
+package com._1zko.hacknovahackathon26backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HacknovaHackathon26BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HacknovaHackathon26BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/_1zko/hacknovahackathon26backend/HacknovaHackathon26BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com._1zko.hacknovahackathon26backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HacknovaHackathon26BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
